ded89ee30f9b51a69c0be3b7b397116330f70336
Mitch Ullman <jmullman@gmail.com>
2016-07-31 22:55:00 -0400

    Added file repeat-a-string-repeat-a-string.js
    This essentially multiplies the String, str, num times and concatenates them.

---
+++ repeat-a-string-repeat-a-string.js
@@ -0,0 +1,18 @@
+
+/*
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+    Global String Object
+*/
+
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  return str;
+}
+
+repeatStringNumTimes("abc", 3);
