9eaa09f4a410c688494390b4a892a304ffb6d21c
jmullman <jmullman@gmail.com>
2016-08-14 12:13:45 -0400

    ChunkyMonkey.js added
    This is currently not operational, but is an initial implementation that is meant to take an array, split it up into chunks the size of value of the size variable. Handling of instances wherein the array does not equally divide into chunks of size-sized sub-arrays should place the remainder into its own sub-array at the end of the matrix.

---
+++ ChunkyMonkey.js
@@ -0,0 +1,42 @@
+/*
+
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+Here are some helpful links:
+
+    Array.prototype.push()
+
+    Array.prototype.slice()
+
+*/
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  twoDiArr = [];
+  for (var i = 0; i < size; i++) {
+    // twoDiArr.push(arr.slice(0,size));
+    if (arr.length <= 0) {
+      console.log("This is inside the if statement\n");
+      return twoDiArr;
+    } else {
+      console.log(arr + ' before slice and splice\n');
+      twoDiArr.push(arr.slice(0,size));
+      arr.splice(0,size);
+    }
+  }
+  return twoDiArr;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+
+/*
+console.log('chunkArrayInGroups(["a", "b", "c", "d"], 2) ' + chunkArrayInGroups(["a", "b", "c", "d"], 2) + '\n');
+console.log("chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) " + chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) + '\n');
+console.log("chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) " + chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) + '\n');
+console.log("chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) " + chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) + '\n');
+console.log("chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) " + chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) + '\n');
+*/
